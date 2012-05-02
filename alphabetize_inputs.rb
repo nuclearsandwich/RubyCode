59c9b52ecef85a6235ca9022361670029b085be6
Anthony Severini <FluffyCode@gmail.com>
2012-05-02 10:15:00 -0700

    Couple fixes - before while-looping the sorting conditional

--- alphabetize_inputs.rb
+++ alphabetize_inputs.rb
@@ -1,4 +1,4 @@
-# Version 0.4
+# Version 0.5
 
 # Notes for next version:
 # Maybe make a while loop out of alphabetize 1/2 choice?  If gets != 1 or 2, ask again (until
@@ -22,25 +22,17 @@ while run_me == 1
 			
 			puts "All done alphabetizing."
 			puts big_ass_prompt
-			user_sorting_choice = gets.chomp.to_s
+			user_sorting_choice = gets.chomp.to_i
 			
-			# Thinking this should be changed from an if-statement, to a while-loop - 
-			# otherwise, if the user inputs anything but a 1 or 2, it will exit back
-			# into the while loop, thinking the user wants to put something else into
-			# the array.
-			if user_sorting_choice == "1" or user_sorting_choice == "2"
-				# option 1 currently not working
-				# outputs as if 2 was chosen
-				if user_sorting_choice == 1
-					puts alphabetize_me
-					exit 0
-				# Nicely enough, however, this one now properly works
-				# (don't have to put in 2 twice)
-				else
-					puts alphabetize_me.join(', ')
-					exit 0
-				end
+			# Before making this into a while loop...
+			if user_sorting_choice == 1
+				puts alphabetize_me
+				exit 0
+			elsif user_sorting_choice == 2
+				puts alphabetize_me.join(', ')
+				exit 0
 			else
+				puts "" # Blank space - easier to read, for now.
 				puts "Input error: expecting either a 1 or a 2."
 				puts big_ass_prompt
 			end
