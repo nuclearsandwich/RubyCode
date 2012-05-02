35d138d6018943423e92c4e9317936c2912b63fd
Anthony Severini <FluffyCode@gmail.com>
2012-05-02 13:49:03 -0700

    Cleaned up comments

--- file_renamer.rb
+++ file_renamer.rb
@@ -1,4 +1,4 @@
-# Version 0.1
+# Version 0.2
 # The program so far - takes an input InThisKindOfFormat, and will turn it into a string in_this_kind_of_format
 # It does not yet change any file names...but that is the ultimate goal.
 
@@ -10,14 +10,13 @@ rename_me = gets.chomp.to_s
 # Will probably want to add a check, later, to see if there is such a file
 # (maybe it doesn't exist - due to user typo)
 
-# The rename method:
 def rename_method str_to_rename
 	# variable x, indexing for the while loop
 	x = 0
 	# rename_progress is the variable storing the progress of the string as the program goes on:
 	rename_progress = ""
 
-	# Define the method that finds if there is a capital letter in the input:
+	# The method that finds if there is a capital letter in the input:
 	def has_uppercase_letters this_capital
 		(this_capital =~ /[A-Z]/) != nil
 	end
@@ -25,16 +24,15 @@ def rename_method str_to_rename
 	while x < str_to_rename.length
 		# Step 1, if the file name starts with a capital letter, we DON'T want an underscore preceding it.
 		if x == 0
-			# So we skip over it...
+			# So we skip over it, but add it to our progress.
 			rename_progress += str_to_rename[x]
-			# ...but add it to our progress.
 		# Step 2a, for each character in the string (after the first), we need to check and see if it a capital letter... 
 		elsif has_uppercase_letters(str_to_rename[x]) == true
 			# We insert an underscore ( _ ) before each capital letter...
 			# ...with .gsub, then include the current indexed character...
-			rename_progress += str_to_rename[x].gsub(/([A-Z])/, ('_' + str_to_rename[x]))
 			# ...and add that to our progress.
-		# Step 2b, we still add in each lower-case character to the rename_progress...
+			rename_progress += str_to_rename[x].gsub(/([A-Z])/, ('_' + str_to_rename[x]))
+		# Step 2b, we still add in each lower-case character, or number, to the rename_progress...
 		else
 			rename_progress += str_to_rename[x]
 		end
@@ -48,7 +46,6 @@ def rename_method str_to_rename
 	# Step 4, we prompt the user as to if the result is what they want:
 	puts "Is '#{rename_progress}' the naming convention you were looking for?"
 	if gets.chomp.downcase == "yes"
-		# rename the file
 		puts "Alrighty - the file name is now #{rename_progress}."
 	else
 		puts "No changes have been made."
