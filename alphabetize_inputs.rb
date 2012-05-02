4b47a5dd47b62e16bc132f2572c988b08d933b76
Anthony Severini <FluffyCode@gmail.com>
2012-05-02 10:40:02 -0700

    Version 1.0 complete. /happyface

--- alphabetize_inputs.rb
+++ alphabetize_inputs.rb
@@ -1,8 +1,10 @@
-# Version 0.5
+# Version 1.0
 
 # Notes for next version:
-# Maybe make a while loop out of alphabetize 1/2 choice?  If gets != 1 or 2, ask again (until
-	# a proper input is given)?
+# Consider putting a way to "exit out" of the "list-creation loop" - in case someone
+	# figures out they still need to add something else
+# Consider putting in a way to allow users to copy/paste in lists (in a certain)
+	# format) - and allow them to add onto it
 
 # This program takes user inputs, puts them into an array,
 # alphabetizes them, then outputs the alphabetized array.
@@ -22,19 +24,20 @@ while run_me == 1
 			
 			puts "All done alphabetizing."
 			puts big_ass_prompt
-			user_sorting_choice = gets.chomp.to_i
+			user_sorting_choice = gets.chomp.to_s
 			
-			# Before making this into a while loop...
-			if user_sorting_choice == 1
-				puts alphabetize_me
-				exit 0
-			elsif user_sorting_choice == 2
-				puts alphabetize_me.join(', ')
-				exit 0
-			else
+			until user_sorting_choice == "1" || user_sorting_choice == "2"
 				puts "" # Blank space - easier to read, for now.
 				puts "Input error: expecting either a 1 or a 2."
 				puts big_ass_prompt
+				user_sorting_choice = gets.chomp.to_s
+			end
+			if user_sorting_choice == "1"
+				puts alphabetize_me
+				exit 0
+			elsif user_sorting_choice == "2"
+				puts alphabetize_me.join(', ')
+				exit 0
 			end
 		else
 			# Anything but "yes" will have the while loop continue to re-run
